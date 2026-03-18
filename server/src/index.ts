042eadbccd58e83e9afbcd96421f765e583d7e03
Eliott-web <he202397@students.ephec.be>
2026-03-18 16:37:32 +0100

    création du message de lancement

---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import { displayBanner } from './startup';
+
+// On affiche la bannière au lancement
+displayBanner();
+
+// Ton code de logique commence ici
+const projetNom: string = "Gestionnaire de Projets Électronique";
+console.log(`Initialisation de ${projetNom} terminée.`);
