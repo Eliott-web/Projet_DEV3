1ff43221ebb558d9f09fdf7ee4a68fc28ea79a32
oumou004 <HE202558@students.ephec.be>
2026-03-27 14:00:47 +0100

    Ajout dans index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
+import express from "express";
 import { displayBanner } from './startup';
 
 // On affiche la bannière au lancement
 displayBanner();
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur notre serveur ");
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
 
 // Ton code de logique commence ici
 const projetNom: string = "Gestionnaire de Projets Électronique";
