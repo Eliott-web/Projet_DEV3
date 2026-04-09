8cce61dc61216cca1e594151e9b4e1b565c9043a
oumou004 <HE202558@students.ephec.be>
2026-04-09 14:14:30 +0200

    Ajout...

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from '../models/project.user';
+// import User from '../models/project.user';
 
 // validation email
 import { isValidEmail } from '../utils/emailValidator';
